7d85d3b5e93039620954086f66ea121e2779c7d5
Kurják Richárd <kurjak.richard@gmail.com>
2020-12-07 10:20:06 +0100

    Add sqlite and xls dependencies

--- src/com/sire/cii/controller/ciiFXMLDocumentController.java
+++ src/com/sire/cii/controller/ciiFXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package canoninvoiceimport;
+package com.sire.cii.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
